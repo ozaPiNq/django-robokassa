3dc81038bb8c3b64b216694d434711218deb3689
Andrey Beletsky <andrey@vr2.net>
2014-12-15 14:43:15 +0300

    Test repair: encoding fix in expected url string

--- robokassa/tests.py
+++ robokassa/tests.py
@@ -25,7 +25,9 @@ class RobokassaFormTest(TestCase):
                          '%s::5:%s:shpparam1=None:shpparam2=None' % (LOGIN, PASSWORD1))
 
     def testRedirectUrl(self):
-        url = "https://merchant.roboxchange.com/Index.aspx?MrchLogin=test_login&OutSum=100.0&InvId=58&Desc=%D5%EE%EB%EE%E4%E8%EB%FC%ED%E8%EA+%22%C1%E8%F0%FE%F1%E0%22&SignatureValue=0EC23BE40003640B35EC07F6615FFB57&Email=vasia%40example.com&shpparam1=None&shpparam2=None"
+        url = "https://merchant.roboxchange.com/Index.aspx?MrchLogin=test_login&OutSum=100.0&InvId=58" \
+        "&Desc=%D0%A5%D0%BE%D0%BB%D0%BE%D0%B4%D0%B8%D0%BB%D1%8C%D0%BD%D0%B8%D0%BA+%22%D0%91%D0%B8%D1%80%D1%8E%D1%81%D0%B0%22" \
+        "&SignatureValue=0EC23BE40003640B35EC07F6615FFB57&Email=vasia%40example.com&Encoding=utf-8&shpparam1=None&shpparam2=None"
         self.assertEqual(self.form.get_redirect_url(), url)
 
 
